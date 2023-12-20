8c36c993a71cd8293019a34d1b1e766af1a3d7c5
unknown <aarongold1869@gmail.com>
2023-12-19 21:22:11 -0600

    home page

--- NiceHouseBro/urls.py
+++ NiceHouseBro/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -6,4 +8,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
