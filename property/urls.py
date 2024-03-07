e4aa084e28c379f6ee02236b3e36a15fd54e3577
unknown <aarongold1869@gmail.com>
2024-03-07 16:04:10 -0600

    explore ui reworked with list passed to template instead of individual property + like and hide css animation

--- property/urls.py
+++ property/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('<str:property_id>/', views.property_detail_view, name='property-detial'),
     path('toggle-desc-visible/<str:property_id>/<str:action>/', views.toggle_property_descripton, name='toggle_desc_visible'),
-    path('toggle-saved/<str:property_id>/', views.toggle_property_saved, name='toggle_saved')
+    path('toggle-saved/<str:property_id>/', views.toggle_property_saved, name='toggle_saved'),
+
+    path('get-explore-controls/<str:property_id>/', views.get_explore_controls_view, name='get_explore_controls'),
+    path('save-property-explore/<str:property_id>/', views.save_property_explore_view, name='save_property_explore'),
 ] 
--- property/views.py
+++ property/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -7,7 +8,6 @@ from account.models import Account, SavedProperty
 from .data import PROPERTY_DATA, Property
 from .functions import get_unliked_properties
 
-import time
 from typing import List
 
 def home_view(request):
@@ -15,40 +15,27 @@ def home_view(request):
     return render(request, 'property/home.html', {'property_list': property_list })
 
 @login_required(login_url='/accounts/login/')
-@require_http_methods(['GET', 'POST'])
 def explore_view(request):
     account = Account.objects.get(user=request.user)
     property_list: List[Property] = get_unliked_properties(account)
-    index = 0
     template = "property/explore.html"
-    data_status = "active"
-    if request.htmx: 
-        index = int(request.POST.get('index', 0))
-        action = request.POST.get('action', None)
-        template = "property/partials/property-card.html"
-        if action == 'like':
-            property_id = request.POST.get('property_id')
-            SavedProperty.objects.create(account=account, property_id=property_id)
-            # return render(request, "property/partials/button-saved.html", {'property': property_list[index] })
-            # property_list: List[Property] = get_unliked_properties(account)
-        elif action == 'unlike':
-            property_id = request.POST.get('property_id')
-            SavedProperty.objects.filter(Q(account=account) & Q(property_id=property_id)).delete()
-            # return render(request, "property/partials/button-unsaved.html", {'property': property_list[index] })
-            # property_list: List[Property] = get_unliked_properties(account)
-        elif action == 'next':
-            data_status = "next"
-            index += 1
-        elif action == 'back':
-            data_status = "prev"
-            index -= 1
-        if index >= len(property_list):
-            if len(property_list) == 0:
-                template = "property/partials/end-of-list.html"
-            else:
-                index = 0
-    property = property_list[index] if index < len(property_list) else None
-    return render(request, template, {'property': property, 'index': index, "data_status": data_status})
+    property_id = property_list[0]['id'] if property_list else -1
+    return render(request, template, {'property_list': property_list, 'property_id': property_id })
+
+@require_http_methods(['GET'])
+def get_explore_controls_view(request, property_id: str):
+    return render(request, 'property\partials\explore-controls.html', {'property_id': property_id })
+
+@login_required(login_url='/accounts/login/')
+@require_http_methods(['POST'])
+def save_property_explore_view(request, property_id: str):
+    # property: Property = next((x for x in PROPERTY_DATA if x['id'] == property_id), None)
+    account = Account.objects.get(user=request.user)
+    SavedProperty.objects.get_or_create(
+        account=account,
+        property_id=property_id
+    )
+    return HttpResponse(status=201)
 
 def property_detail_view(request, property_id: str):
     property: Property = next((x for x in PROPERTY_DATA if x['id'] == property_id), None)
