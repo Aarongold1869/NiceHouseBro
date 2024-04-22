71e54608889d81a52a6215276d59c79d27d890d5
aarongold1869 <aarongold1869@gmail.com>
2024-04-22 10:44:24 -0500

    redfin scraper class refactor

---
+++ property/api/__init__.py
@@ -0,0 +1,2 @@
+from redfin.scraper import RedfinScraper
+from redfin.redfin_types import Property
---
+++ property/api/redfin/scraper.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+from typing import List
+import urllib
+
+from redfin_types import Property
+
+class RedfinScraper():
+
+    def __init__(self, include_nearby_homes: bool, market:str, num_homes:int, page_number:int, poly:str):
+        self.al = 1
+        self.include_nearby_homes = include_nearby_homes
+        self.market = market
+        self.num_homes = num_homes
+        self.ord = 'redfin-recommended-asc'
+        self.page_number = page_number
+        self.poly = poly
+        self.sf = '1,2,3,5,6,7'
+        self.start = 0
+        self.status = 1
+        self.uipt = '1,2,3,4,5,6,7,8'
+
+    def get_endpoint(self)-> str:
+        endpoint = 'gis?' + urllib.parse.urlencode(self.__dict__, safe=',%').lower()                                                                                                                          #-122.54472%2047.44109%2C-122.11144%2047.44109%2C-122.11144%2047.78363%2C-122.54472%2047.78363%2C-122.54472%2047.44109'
+        return endpoint
+    
+    def get_property_list(self)-> List[Property]:
+        base_url = 'https://www.redfin.com/stingray/api/'
+        url = base_url + self.get_endpoint()
+        headers={  
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)", 
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            pretty = soup.prettify().replace('{}&amp;&amp;', '')
+            data = json.loads(pretty)["payload"]["homes"]
+            return data
+        else:
+            print(response.status_code)
+            print('Failed to scrape the URL')
+            return []
+    
+def main():
+    scraper = RedfinScraper(
+        include_nearby_homes=True, 
+        market='seattle', 
+        num_homes=1,
+        page_number=1, 
+        poly='-122.54472%2047.44109%2C-122.11144%2047.44109%2C-122.11144%2047.78363%2C-122.54472%2047.78363%2C-122.54472%2047.44109'
+    )
+    data: List[Property] = scraper.get_property_list()
+    if len(data) > 0:
+        print(data)
+
+if __name__ == '__main__':
+    main()
+
+
